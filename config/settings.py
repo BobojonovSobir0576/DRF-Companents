d9af1a96412914729599173aa8a3fc8a5fced2c1
BobojonovSobir0576 <dweb0987654321@gmail.com>
2023-11-24 20:06:56 +0500

    dfdfdf

--- config/settings.py
+++ config/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "companents",
         "USER": "postgres",
-        "PASSWORD": "0576",
+        "PASSWORD": "1",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
